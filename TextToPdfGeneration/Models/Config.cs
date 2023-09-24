4496e00ca69cc6cc03c6fce41e0b05dfadbe51bf
Hussain <hussain.binginapalli@gmail.com>
2023-09-25 00:35:56 +0530

    Initial commit

---
+++ TextToPdfGeneration/Models/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToPdfGeneration.Models
+{
+    internal class Config
+    {
+        public string[] FieldList { get; set; }
+        public string PdfFileContentFormat { get; set; }
+        public string OutputFileNameLookup { get; set; }
+        public string Seperator { get; set; }
+    }
+}
